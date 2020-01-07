6fb1d5fac16d3f05f9b5525d36e4186ba8216491
phil-park <jw.phil.park@gmail.com>
2020-01-07 19:43:57 +0900

    add convention

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,14 @@
-import java.net.URI
-
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.util.ToolRunner
-import sinotify.{Launcher, Listener}
+import sinotify.Launcher
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     val hadoopConf = new Configuration()
     val runner = ToolRunner.run(hadoopConf, Launcher, args)
+    sys.exit(0)
   }
-
-
 }
 
 
